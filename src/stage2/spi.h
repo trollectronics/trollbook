293391807c579cca01a2399fcd47e1b021668571
Axel Isaksson <isaksson.axel@telia.com>
2016-09-10 02:50:37 +0200

    ported trollcard SD card code

--- src/stage2/spi.h
+++ src/stage2/spi.h
@@ -6,6 +6,19 @@
 #define SPI_REG_STATUS *((volatile uint32_t *) 0x200004UL)
 #define SPI_REG_SS *((volatile uint32_t *) 0x200008UL)
 
+typedef enum SpiSlave SpiSlave;
+
+enum SpiSlave {
+	SPI_SLAVE_NONE,
+	SPI_SLAVE_ROM,
+	SPI_SLAVE_TGB,
+	SPI_SLAVE_SD,
+	SPI_SLAVE_EXT0,
+	SPI_SLAVE_EXT1,
+	SPI_SLAVE_EXT2,
+	SPI_SLAVE_EXT3,
+};
+
 void spi_select_slave(int slave);
 uint8_t spi_send_recv(uint8_t dat);
 void spi_set_clockdiv(uint16_t clkdiv);
